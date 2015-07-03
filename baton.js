c39bf3cc3341ac7ed1696e904cef7d1f2dd462f6
eaton11 <sameaton11@gmail.com>
2015-07-02 20:11:29 -0600

    adds ability to pass any number of arguments

--- baton.js
+++ baton.js
@@ -1,17 +1,20 @@
 function baton(_init){
 	var b = {
-		run:function(_input){
+		run:function(){
+			var args = Array.prototype.splice.call(arguments, 0);
 			b.__utils__.i = -1;
-			return b.yield((_input!==undefined)?_input:null)
+			return b.yield.apply(null, args);
 		},
 		then: function(_callback){
 			if(typeof _callback === "function") b.__utils__.queue.push(_callback);
 			return b;
 		},
 		yield: function(_data){
+			var args = Array.prototype.splice.call(arguments, 0);
+			args.unshift(b.yield);
 			if(typeof b.__utils__.queue[b.__utils__.i+1] !== "undefined"){
 				b.__utils__.i++;
-				return b.__utils__.queue[b.__utils__.i](b.yield, _data);
+				return b.__utils__.queue[b.__utils__.i].apply(null, args);
 			}
 			return _data;
 		},
